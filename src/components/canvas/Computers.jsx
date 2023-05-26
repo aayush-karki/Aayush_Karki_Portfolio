08167ba30e72882746eada435d96cf7645d1cf7d
Aayush Karki <akarki@ramapo.edu>
2023-05-26 14:44:58 -0400

    creates canvasLoader component

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -37,8 +37,7 @@ const ComputersCanvas = () => {
 			camera={{ position: [20, 3, 5], fov: 25 }}
 			gl={{ preserveDrawingBuffer: true }}
 		>
-			{/* <Suspense fallback={<CanvasLoader />}> */}
-			<Suspense>
+			<Suspense fallback={<CanvasLoader />}>
 				<OrbitControls
 					enableZoom={false}
 					maxPolarAngle={Math.PI / 2}
