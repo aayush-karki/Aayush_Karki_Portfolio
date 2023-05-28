182402b100de0fb07dcf07c63eb518b3b631ff4c
Aayush Karki <akarki@ramapo.edu>
2023-05-27 20:35:53 -0400

    creates tech component

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import { technologies } from "../constants";
+
+import { BallCanvas } from "./canvas";
+import { SectionWraper } from "../hoc";
 
 const Tech = () => {
-  return (
-    <div>Tech</div>
-  )
-}
+	return (
+		<div className="flex flex-row flex-wrap justify-center gap-10">
+			{technologies.map((tech) => (
+				<div className="w-28 h-28" key={tech.name}>
+					<BallCanvas icon={technologies.icon} />
+				</div>
+			))}
+		</div>
+	);
+};
 
-export default Tech
+const TechWapper = () => {
+	return (
+		<SectionWraper idName="work">
+			<Tech />
+		</SectionWraper>
+	);
+};
+
+export default TechWapper;
